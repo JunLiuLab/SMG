0923b2131fbe2fce94d5d1efa77a70325684d7a9
lyc105 <36849197+lyc105@users.noreply.github.com>
2021-07-19 07:56:04 +0800

    Update smg_varypara.sh

--- smg_varypara.sh
+++ smg_varypara.sh
@@ -2,9 +2,9 @@ for PARAM1 in 20; do for RHO in 100; do for NT in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 
 #
 export PARAM1 RHO DIM NT COR TYPE
 #
-sbatch -o outputs/oSMG_${PARAM1}_${RHO}_${DIM}_${NT}.stdout.txt \
--e outputs/eSMG_${PARAM1}_${RHO}_${DIM}_${NT}.txt \
---job-name=SMG_${PARAM1}_${RHO}_${DIM}_${NT} \
+sbatch -o outputs/oSMG_${PARAM1}_${RHO}_${NT}.stdout.txt \
+-e outputs/eSMG_${PARAM1}_${RHO}_${NT}.txt \
+--job-name=SMG_${PARAM1}_${RHO}_${NT} \
 smg.sh
 #
 sleep 1
